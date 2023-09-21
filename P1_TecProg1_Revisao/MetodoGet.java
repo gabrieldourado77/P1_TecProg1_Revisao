d7fad61454097ece42d2d616dd4342ad0fd2cb45
Gabriel Dourado dos Santos <82122849+gabrieldourado77@users.noreply.github.com>
2023-09-21 18:20:27 -0300

    Delete P1_TecProg1_Revisao directory

--- P1_TecProg1_Revisao/MetodoGet.java
+++
@@ -1,7 +0,0 @@
-public class MetodoGet {
-    
-    /*
-        Método Get - Colocarei a explicação em breve.
-    */
-
-}
--- P1_TecProg1_Revisao/MetodoSet.java
+++
@@ -1,7 +0,0 @@
-public class MetodoSet {
-    
-    /*
-        Método Set - Colocarei a explicação em breve.
-    */
-
-}
